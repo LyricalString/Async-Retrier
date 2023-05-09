65acede43953e5498bc40a68d17ebbe6705f0568
Alex Martinez Jorge <lyricalstring@outlook.es>
2023-05-09 14:21:53 +0200

    Added new JSDoc and secured that the function parameter is a function

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
 import { AsyncRetryOptions } from '../types';
 
 /**
- * A function that retries an asynchronous function until it succeeds, or until
- * the maximum number of retries is reached.
- *
- * @param fn The asynchronous function to retry.
- * @param options The retry options.
- * @returns The result of the asynchronous function.
- */
+  Retries an asynchronous function a set number of times until successful or until the maximum number of retries is reached.
+  @template T
+  @param {() => Promise<T>} fn - The asynchronous function to retry.
+  @param {AsyncRetryOptions} [options={}] - The retry options to use.
+  @param {number} [options.maxRetries=3] - The maximum number of times to retry the function.
+  @param {number} [options.delay=1000] - The delay in milliseconds between each retry attempt.
+  @param {(error: any, retries: number, delay: number) => void} [options.onError=() => {}] - The function to call on each retry attempt when an error occurs.
+  @returns {Promise<T>} The result of the successful function call.
+  @throws {Error} If the function is not provided or if the maximum number of retries is exceeded.
+*/
 export const asyncRetry = async <T>(
   fn: () => Promise<T>,
   options: AsyncRetryOptions = {}
 ): Promise<T> => {
+  if (typeof fn !== 'function') {
+    throw new Error('asyncRetry function must be provided a function');
+  }
   let retries = 0;
   const maxRetries = options.maxRetries || 3;
   const delay = options.delay || 1000;
-  const onError = options.onError || (() => {});
+  const onError = options.onError || (() => { });
   while (retries < maxRetries) {
     try {
       return await fn();
